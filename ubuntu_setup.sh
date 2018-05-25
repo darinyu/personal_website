870aef49b8789f08a2e848ff6b188b40a821757f
Darin Yu <darinyu27@gmail.com>
2018-05-24 21:34:28 -0700

    helloworld app with expressjs

---
+++ ubuntu_setup.sh
@@ -0,0 +1,8 @@
+sudo apt-get update && sudo apt-get -y upgrade
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
+sudo apt-get install -y nodejs
+sudo apt-get install -y npm
+npm install nodemon -g
+
+# in website folder
+# npm install
--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,43 @@
-const path = require("path");
-const webpack = require("webpack");
-const bundlePath = path.resolve(__dirname, "dist/");
 
-module.exports = {
-  entry: "./src/index.js",
-  module: {
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, './build');
+var APP_DIR = path.resolve(__dirname, './src/client');
+
+const config = {
+   entry: {
+     main: APP_DIR + '/index.js'
+   },
+   output: {
+     filename: 'bundle.js',
+     path: BUILD_DIR,
+   },
+   module: {
     rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /(node_modules|bower_components)/,
-        loader: 'babel-loader',
-        options: { presets: ['env'] }
-      },
-      {
-        test: /\.css$/,
-        use: [ 'style-loader', 'css-loader' ]
-      }
-    ]
-  },
-  resolve: { extensions: ['*', '.js', '.jsx'] },
-  output: {
-    publicPath: bundlePath,
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: path.join(__dirname,'public'),
-    port: 3000,
-    publicPath: "http://localhost:3000/dist"
-  },
-  plugins: [ new webpack.HotModuleReplacementPlugin() ]
+     {
+       test: /(\.css|.scss)$/,
+       use: [{
+           loader: "style-loader" // creates style nodes from JS strings
+       }, {
+           loader: "css-loader" // translates CSS into CommonJS
+       }, {
+           loader: "sass-loader" // compiles Sass to CSS
+       }]
+     },
+     {
+       test: /\.(jsx|js)?$/,
+       use: [{
+         loader: "babel-loader",
+         options: {
+           cacheDirectory: true,
+           presets: ['react', 'es2015'] // Transpiles JSX and ES6
+         }
+       }]
+     }
+    ],
+
+  }
 };
+
+module.exports = config;
